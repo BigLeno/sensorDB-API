2f320800ebd200ce9526d0a6c9ac5389eef239a7
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2024-03-08 00:33:39 -0300

    Rotas novas adicionadas
    O histórico está adicionado, porém, está com algo quebrado

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,30 @@ import express from 'express';
 import prisma from './prisma';
 
 const app = express();
+const port = 3000;
 
-app.get('/temperature-now', async (req, res) => {
+app.get('/last-temperature', async (req, res) => {
+  const last_temperature = await prisma.info.findFirst({
+    orderBy : { id: "desc"}
+  })
+  res.send({"last": last_temperature?.temperature})
+})
+
+app.get('/last-humidity', async (req, res) => {
+  const last_humidity = await prisma.info.findFirst({
+    orderBy : { id: "desc"}
+  })
+  res.send({"last": last_humidity?.humidity})
+})
+
+app.get('/last-luminosity', async (req, res) => {
+  const last_luminosity = await prisma.info.findFirst({
+    orderBy : { id: "desc"}
+  })
+  res.send({"last": last_luminosity?.luminosity})
+})
+
+app.get('/daily-temperature', async (req, res) => {
     const currentDay = new Date();
     currentDay.setHours(0,0,0,0);
     const currentDayFinal = new Date();
@@ -18,7 +40,7 @@ app.get('/temperature-now', async (req, res) => {
   res.send({"media": temperature._avg.temperature});
 });
 
-app.get('/humidity-now', async (req, res) => {
+app.get('/daily-humidity', async (req, res) => {
     const currentDay = new Date();
     currentDay.setHours(0,0,0,0);
     const currentDayFinal = new Date();
@@ -33,7 +55,7 @@ app.get('/humidity-now', async (req, res) => {
   res.send({"media": humidity._avg.humidity});
 });
 
-app.get('/luminosity-now', async (req, res) => {
+app.get('/daily-luminosity', async (req, res) => {
     const currentDay = new Date();
     currentDay.setHours(0,0,0,0);
     const currentDayFinal = new Date();
@@ -48,7 +70,39 @@ app.get('/luminosity-now', async (req, res) => {
   res.send({"media": luminosity._avg.luminosity});
 });
 
+app.get('/historic', async (req, res) => {
+  const {page, total, createdAt, maxTemp, minTemp, maxHumidity, minHumidity, maxLuminosity, minLuminosity} = req.query
+  const currentDay = new Date(`${createdAt}`)
+  currentDay.setHours(0,0,0,0)
+  const currentDayFinal = new Date(`${createdAt}`)
+  currentDayFinal.setHours(23,59,59,999);
+  const historic_data = await prisma.info.findMany({
+    where : {
+      createdAt : {
+        gte:currentDay,
+        lte:currentDayFinal
+      },
+      temperature : {
+        gte : Number(minTemp),
+        lte : Number(maxTemp)
+      },
+      humidity : {
+        gte : Number(minHumidity),
+        lte : Number(maxHumidity)
+      },
+      luminosity : {
+        gte : Number(minLuminosity),
+        lte : Number(maxLuminosity)
+      }
+    },
+    orderBy : {createdAt: "desc"},
+    skip : Number(page) * Number(total),
+    take : Number(total)
+  })
+  res.send({"historic" : historic_data})
+})
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
